880f96c8ca6bac1e337ab44063c462190098fd2d
Adam Mathis <adam.mathis05@gmail.com>
2024-06-25 08:59:54 -0500

    practice files push

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,74 @@
 
 //     sum = sum + myGrades[i];
 
+
+// JS PROMISES
+
+// let p = new Promise((resolve, reject) => {
+// let a = 1 + 2
+// if (a == 2) {
+//     resolve('Success')
+
+// }
+// else reject('Failed')
+
+// })
+
+// p.then((message)=> {
+//     console.log('This is in the then ' + message)
+// }).catch((message) => {
+//     console.log('This is in the catch ' + message)
+// })
+
+// JS ARROW FUNCTIONS
+
+// function sum(a, b) {
+//     return a + b
+// }
+
+// let sum2 = (a, b) => a + b
+
+
+// function isPositive(number) {
+//     return number >= 0
+// }
+
+// let isPositive2 = number => number >= 0 
+
+// function randomNum() {
+//     return Math.random
+// }
+
+// let randomNum2 = () => Math.random
+
+// document.addEventListener('click', function() {
+//     console.log('Click');
+// })
+
+// document.addEventListener('click', () => console.log('Click'))
+
+// class Person {
+//     constructor(name) {
+//         this.name = name
+//     }
+
+//     printNameArrow() {
+//         setTimeout(() => {
+//             console.log('Arrow: ' +this.name );
+//         }, 100)
+//     }
+
+//     printNameFunction() {
+//         setTimeout(function() {
+//             console.log('Function: ' + this.name)
+//         }, 100)
+//     }
+// }
+
+// let person = new Person('Bob')
+// person.printNameArrow()
+// person.printNameFunction()
+
+
+// 
+
